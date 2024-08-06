7c9f67d710aea2fde41b9a73e5cc2f9dacb1576b
Gabriele Toffanin <GabrieleToffanin@outlook.it>
2024-08-06 18:20:45 +0200

    chore: Add support for string concatenation and fix memory management

--- compiler.h
+++ compiler.h
@@ -1,6 +1,9 @@
 #ifndef turbo_compiler_h
 #define turbo_compiler_h
+
+#include "object.h"
 #include "vm.h"
+
 bool compile(const char* source, Chunk* chunk);
 
 #endif
--- vm.h
+++ vm.h
@@ -11,6 +11,8 @@ typedef struct {
   uint8_t* ip; // represent virtual InstructionPointer
   Value stack[STACK_MAX];
   Value* stack_top;
+
+  Obj* objects;
 } VM;
 
 typedef enum{
@@ -18,6 +20,8 @@ typedef enum{
   INTERPRET_COMPILE_ERROR,
   INTERPRET_RUNTIME_ERROR
 } InterpretResult;
+
+extern VM vm;
 
 void init_vm();
 void free_vm();
